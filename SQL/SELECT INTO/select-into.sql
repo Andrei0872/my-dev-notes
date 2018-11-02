fd2ba66103eda6489627ad47c80e13baa9d2a25d
Andrei0872 <andreigtj01@gmail.com>
2018-11-03 00:29:32 +0200

    added SQL

---
+++ SQL/SELECT INTO/select-into.sql
@@ -0,0 +1,11 @@
+
+-- https://codeburst.io/mysql-select-into-syntax-part-3-dumpfile-with-examples-d261f6c8fe7e
+-- https://dev.mysql.com/doc/refman/5.7/en/select-into.html
+
+--* enables a query result to be store in variables or a written file
+-- SELECT ...  INTO
+-- {
+--    var_list : selects column vals and stores them into variables
+--    outfile - writes the selected row to a file - formatted
+--    dumpfile - writes a single row to the file without any formatting
+-- }
