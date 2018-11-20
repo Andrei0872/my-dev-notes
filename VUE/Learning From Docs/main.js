751034ccdb49554afa1e2091874af8007e88cf28
Andrei0872 <andreigtj01@gmail.com>
2018-11-20 13:32:51 +0200

    current lesson: Class and Style bindings

--- VUE/Learning From Docs/main.js
+++ VUE/Learning From Docs/main.js
@@ -80,7 +80,16 @@ var app7 = new Vue({
             { id: 0, text: 'Vegetables' },
             { id: 0, text: 'Cheese' },
             { id: 0, text: 'Whatever else humans are supposed to eat' }
-        ]
+        ],
+        msg: "This is a message",
+        rawHTML:'<span style="color:red;">This should be red</span>',
+        isButtonDisabled: true,
+        ok: true
+    },
+    methods: {
+        showAlert: function () {
+            alert(1);
+        }
     }
 });
 
@@ -112,5 +121,102 @@ new Vue({
 });
 
 // ===========================================
+//* Computed Properties and Watchers
+// The value of `vm.reversedMessage` is always dependent on `vm.message`
+var app8 = new Vue({
+    el: "#app-8",
+    data: {
+        message: 'hello',
+        fname: 'Foo',
+        lname: 'Bar',
+        fullname: 'Foo Bar'
+    },
+    computed: {
+        // A computed getter
+        reversedMessage: function() {
+            // "this" - points to the vm instance
+            return this.message.split``.reverse().join``;
+        },
+        fullNameComputed: function () {
+            return `${this.fname} ${this.lname}`;
+        },
+        // Computed Setter
+        fullNameUsingSetter: {
+            // Getter
+            get: function () {
+                return `${this.fname} ${this.lname}`;
+            },
+            // Setter
+            set: function (newValue) {
+                var names =  newValue.split` `;
+                this.fname = names[0];
+                this.lname = names[names.length - 1];
+            }
+        }
+    },
+    methods: {
+        reverseMessage: function() {
+            return this.message.split``.reverse().join``;
+        }
+    },
+    watch: {
+        fname: function(val, oldVal) {
+            this.fullname = `${val} ${this.lname}`;
+            // console.log('fname old', oldVal)
+        },
+        lname: function(val, oldVal) {
+            this.fullname = `${this.fname} ${val}`;
+            // console.log('lname old', oldVal)
+        }
+    }
+});
+
 // ===========================================
+//* Watcher example
+
+var app9 = new Vue({
+    el: "#watcher-example",
+    data : {
+        question: '',
+        answer: 'I cannot give you an answer until you ask a question!'
+    },
+    watch: {
+        // Whenever 'question' changes, this function will run
+        question: function(newQuestion, oldQuestion) {
+            this.answer = 'Waiting for you to stop typing...';
+            this.debouncedGetAnswer();
+        }
+    },
+    beforeCreate: function() {
+        console.log("Vue Instance before created") // This will log first
+    },
+    // When the Vue Instance is created
+    created: function() {
+        console.log("Vue Instance created") // This will log second
+        // _.debounce - limit how ofter a particularly expensive operation can be run
+        // In this case, we want to limit how ofter we access yesno.wtf/api
+        // waiting until the user has completely finished typing before making the ajax request
+        this.debouncedGetAnswer = _.debounce(this.getAnswer,500);
+    },
+    methods: {
+        getAnswer: function() {
+            console.log("The user has stopped typing")
+            if(this.question.indexOf('?') === -1) {
+                this.answer = 'Questions usually contain a question mark. ;-)';
+                return;
+            }
+            this.answer = 'Thinking...';
+            var vm = this
+            axios.get('https://yesno.wtf/api')
+                .then(function(response) {
+                    vm.answer = _.capitalize(response.data.answer);
+                })
+                .catch(function(err) {
+                    vm.answer = 'Error! Could not reach the API. ' + error
+                });
+        }
+    }
+});
+
+
 // ===========================================
--- VUE/notes.md
+++ VUE/notes.md
@@ -58,3 +58,45 @@ Vue's instance properties and methods : prefixed with $
 lifecycle hooks - give users the opportunity to add their own code at specific stages
 
 all lifecycles hooks are called with their ```this``` context
+
+---
+
+v-once :  do not update on data change
+
+---
+
+v-bind : add attributes
+
+---
+
+you should not attempt to access user-defined globals in template expressions
+
+---
+
+### Directives
+
+prefixed by ```v-```
+
+its job is to ```reactively apply side effects to the DOM``` when the value of its expr changes
+
+some directives can take an ```argument```, denoted by ```a colon``` after the directive name
+```v-bind``` - used to reactively update an html attribute
+
+---
+
+### Computerd Properties and Watchers
+
+computed properties are ```cached``` based on their dependencies
+```computed prop ```- will only ```re-evaluate``` when some of ```its dependencies have changed```
+
+```method``` -  will ```always``` run the function whenever a re-render happens
+
+#### Computed vs Watched
+
+when you have data that needs to change based on some other data: use ```watch```
+however, it is ofter a ```better idea``` to use ```computed properties```
+
+#### Watchers
+
+useful when you want to perfom asynchronous or expensive operations in respons to changing data
+
