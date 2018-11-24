83f4dbc8332a8b8b1668f03f691fe7c5448336c3
Andrei0872 <andreigtj01@gmail.com>
2018-11-25 01:08:50 +0200

    add a new way to compute fibonacci numbers

--- JavaScript/Generators/understaning-generators.js
+++ JavaScript/Generators/understaning-generators.js
@@ -19,3 +19,23 @@ console.log(fib.next().value) // 13
 console.log(fib.next().value) // 21
 console.log(fib.next().value) // 34
 console.log(fib.next().value) // 55
+
+
+// ==================================
+
+let fibonacci2 = {
+    [Symbol.iterator]() {
+        let prev = 0, cur = 1
+        return {
+            next() {
+                [prev, cur] = [cur, prev + cur]
+                return { done:false, value: cur }
+            }
+        }
+    }
+}
+
+for(let n of fibonacci2) {
+    if(n > 1000) break;
+    console.log(n) //  1 2 3 5 8 13 21 34 55 89 144 233 377 610 987
+}
