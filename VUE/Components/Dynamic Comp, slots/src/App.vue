76ab00a0e7ad3bc38f4b84908f5319c226414f4a
Andrei0872 <andreigtj01@gmail.com>
2018-11-27 20:28:11 +0200

    concepts regarding components

---
+++ VUE/Components/Dynamic Comp, slots/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p> {{ selectedComponent }}</p>
+                <!-- <app-quote>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p slot="content">A wonderful Quote</p>
+                </app-quote> -->
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote';
+    import Author from './components/Author';
+    import New from './components/New';
+
+    export default {
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        },
+        data() {
+            return {
+                quoteTitle: 'This is the Quote',
+                selectedComponent: 'appQuote'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ VUE/Components/Dynamic Comp, slots/src/components/Author.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3>The author</h3>
+    </div>
+</template>
+
+<script>    
+    export default {
+    }
+</script>
+
+<style>
+</style>
+
