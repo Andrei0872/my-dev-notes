87f0d22ae11e02a1da9e844c909b5b97285f0ffb
Andrei0872 <andreigtj01@gmail.com>
2018-11-29 16:12:25 +0200

    add app quote

---
+++ VUE/app-quote/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+    import Footer from './components/Footer.vue';
+
+    export default {
+        data: function () {
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete Quotes first!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header,
+            appFooter: Footer
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ VUE/app-quote/components/Quote.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-lg-3 col-sm-6 col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+    .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: #6e6e6e;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:hover {
+        background-color: #ffe2e2;
+    }
+</style>
