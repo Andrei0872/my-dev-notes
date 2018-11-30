00674af9c64bfc6977db282f385d2817f3023ff9
Andrei0872 <andreigtj01@gmail.com>
2018-12-01 01:51:11 +0200

    small quiz app

---
+++ VUE/Transitions and Animations/quiz-app/components/Answer.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div class="panel panel-success">
+        <div style="font-weight:bold;" class="panel-heading text-center">
+            Well Done
+        </div>
+        <div class="panel-body text-center">
+            <button @click="resetGame" class="btn btn-primary">Keep Playing!</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            resetGame() {
+                this.$emit('resetGame', true);
+            }
+        }
+    }
+</script>
---
+++ VUE/Transitions and Animations/quiz-app/components/Question.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <div class="panel panel-default">
+        <div style="font-weight:bold;" class="panel-heading text-center">
+            {{ question }}
+        </div>
+        <div class="panel-body">
+            <div 
+                class="col-xs-12 col-sm-6 text-center" 
+                v-for="(result, index) in results"
+                :key="index"
+                >
+                <button @click="checkValidity(result)" class="btn btn-primary">{{ result }}</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['question' , 'results'],
+        methods: {
+            checkValidity(value) {
+                this.$emit('checkButton', value);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    button {
+        margin: 3px 5px;
+    }
+</style>
