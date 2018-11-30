f4b20d605dcefa7620d2eb5c7ae5cc37eae29cbf
Andrei0872 <andreigtj01@gmail.com>
2018-11-30 19:02:42 +0200

    filters and mixins

---
+++ VUE/Filters and Mixins/Exercise/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <input type="text" v-model="firstEx" placeholder="Reverse...">
+                <p>{{ firstEx | reverse }}</p>
+                <hr>
+                <input type="text" v-model="secondEx" placeholder="show text and length">
+                <p>{{ secondEx | showLengh }}</p>
+                <hr>
+                <p> {{ reversed_computed }} </p>
+                <p> {{ showLengh_computed }} </p>
+                <hr>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { myMixin } from './myMixin.js';
+
+    export default {
+        mixins: [myMixin],
+        filters: {
+            reverse(value) {
+                return value.split``.reverse().join``;
+            },
+            showLengh(value) {
+                return value.length !== 0 ?  `${value} ${value.length}` : '';
+            }
+        },
+        data() {
+            return {
+                firstEx: '',
+                secondEx: ''
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ VUE/Filters and Mixins/List.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Filters & Mixins</h1>
+        <hr>
+        <input type="text" v-model='filterText'>
+        <ul>
+            <li
+                v-for="(fruit, index) in filteredFruits"
+                :key="index"
+            >{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { fruitMixin } from './fruitMixin.js';
+
+    export default {
+        mixins: [fruitMixin],
+        created() {
+            console.warn('list comp created');
+        }        
+    }
+</script>
+
+<style>
+</style>
