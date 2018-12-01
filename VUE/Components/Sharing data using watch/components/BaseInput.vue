9109c53eb55d50cb13396172f4540af40d42a48b
Andrei0872 <andreigtj01@gmail.com>
2018-12-01 03:58:40 +0200

    sharing data using watch

---
+++ VUE/Components/Sharing data using watch/components/BaseInput.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <div class="panel panel-default">
+        <div style="font-weight:bold;" class="panel-heading text-center">
+            Add Item
+        </div>
+        <div class="panel-body">
+            <input 
+                type="text"
+                @keyup.enter="sendData"
+                v-model="itemText"
+                placeholder="type item here"
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                itemText: ''
+            }
+        },
+        methods: {
+            sendData() {
+                this.$emit('data-sent', this.itemText)
+                this.itemText = '';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
