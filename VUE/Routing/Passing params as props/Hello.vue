df9a59b644e610037cad5efd40ef552abd38fbc5
Andrei0872 <andreigtj01@gmail.com>
2018-12-01 22:53:10 +0200

    passing params as props

---
+++ VUE/Routing/Passing params as props/Hello.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div>
+    <h2 class="hello">Hello {{name}} {{ JSON.stringify($attrs) === JSON.stringify({}) ? '' : $attrs }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'Vue!'
+    }
+  },
+  data() {
+      return {
+          ok: false
+      }
+  }
+}
+</script>
