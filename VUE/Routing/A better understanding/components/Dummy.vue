2b3decd54cac86f55f38cefa2f65fecac4bf7d61
Andrei0872 <andreigtj01@gmail.com>
2018-12-14 13:16:19 +0200

    trying some stuff

---
+++ VUE/Routing/A better understanding/components/Dummy.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h3>Dummy Comp {{ $route.params.slug || 'no param' }}</h3>
+        <p>Props Passed : {{ action || 'no action' }}</p>
+        <p>Switch to test comp</p>
+        <button @click="switchToTest()">Switch</button>
+        <button @click="switchToTest('left')">Left Arrow</button>
+        <button @click="switchToTest('right')">Right Arrow</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            switchToTest(direction = 'default') {
+                direction === 'default' && this.$router.push('/test') ||
+                direction === 'left' && this.$router.go(-1) ||
+                direction === 'right' && this.$router.go(1) 
+            }
+        },
+        beforeRouteLeave (to, from, next) {
+            console.log('leaving')
+            next()
+        },
+        props: ['action']
+    }
+</script>
