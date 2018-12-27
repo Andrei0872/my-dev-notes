e5007dc4129861ac36155dba69eaa7a815ab87f9
Andrei0872 <andreigtj01@gmail.com>
2018-12-27 02:41:57 +0200

    streams - introduction

---
+++ Node.js/Streams/stream1.js
@@ -0,0 +1,96 @@
+
+
+/**
+ ** Streams
+ * 
+ * handle reading/writing files, network communications, any end-to-end information exchange
+ * 
+ * traditional way(of reading files): tell the program to read a file, the file is read into memory(from start to finish)
+ * and you process it
+ * 
+ * using streams - you read it piece by piece,//* processing its content without keeping it all in memory
+ * 
+ */
+
+
+//* Advantages
+// Memory efficiency - don't need to load large amounts of data in memory before you are able to process it
+// Time efficiency - less time to start processing data as soon as you have it, rather than waiting till 
+//                   the whole data payload is available to start
+
+
+// ===============================================================
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, resp) => {
+    // fs.readFile(__dirname + '/file.txt', (err, data) => {
+        // resp.end(data);
+    // });
+
+    // If the file is big, the operation will take quite a bit of time
+    // fs.createReadStream() - create a readable stream toa file
+    const stream = fs.createReadStream(__dirname + '/file.txt');
+    // Take the source and pipe it to the destination(source)
+    stream.pipe(resp);
+});
+
+// server.listen(3000);
+
+
+// ===============================================================
+
+// Types of streams
+// Readable - stream you can pipe data from, but not pipe into; when pushing data into a readable stream, it is buffered
+//          - until a consumer stats to read the data
+// Writable - stream you can pipe into, but not pipe from
+// Duplex - both pipe into and pipe from
+// Transform - similar to a Duplex, but the output is transform of its input
+
+// * Getting data from a readable stream
+const Stream = require('stream');
+const readableStream = new Stream.Readable();
+const writableStream = new Stream.Writable();
+
+readableStream.on('readable', () => {
+    console.log(readableStream.read())
+    // console.log(Buffer.concat([readableStream.read()]).toString()) // ​​​​​hi!hi!ho!​​​​​
+})
+
+writableStream._write = (chunk, encoding, next) => {
+    console.log(chunk.toString())
+    next();
+}
+
+readableStream.pipe(writableStream); 
+
+readableStream.push('hi!');
+readableStream.push('hi!');
+readableStream.push('ho!');
+
+
+// writableStream.write('hey!\n');
+
+// When this disabled: console.log(chunk.toString()).
+// writableStream.end()
+
+// =============================================
+
+const Stream = require('stream');
+const readable = new Stream.Readable();
+
+readable.on('data', (chunk, enc, err) => {
+    console.log(Buffer.concat([chunk]).toString()) // 31231 312312
+})
+
+readable.on('close', (msg, data) => {
+    console.log(msg)
+    console.log(data)
+});
+
+readable._read = (data, enc, next) => {}
+
+readable.push('31231')
+readable.push('312312')
+
