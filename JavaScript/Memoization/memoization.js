ef69d6e86adb1bab0349374b2e3f1fd7f75b1f9a
Andrei0872 <andreigtj01@gmail.com>
2019-03-01 23:48:05 +0200

    :sparkles: js memoization

--- JavaScript/Memoization/memoization.js
+++ JavaScript/Memoization/memoization.js
@@ -1,52 +1,26 @@
 
+const memoize = fn => {
+    const cache = {};
 
-// Memoization -  caching function results
-
-// This is a pure function - meaning that it will return the same result if we give the same parameters 
-const factorial = n => {
-    if(n === 1 ) 
-        return 1
-    return n * factorial(n - 1)
-}
-
-
-// console.log(factorial(4)) // 24
-
-// We will use closures
-// Closure -  an inner function that has acces to the outer functions variables
-
-// Memoized function 
-const memFactorial = () => { 
-    
-    // This object is defined inside the memFactorial() function, thus the variable scope is the function scope
-    const cache = {}
-
-    // Return a function
-    return (value) => {
+    return num => {
+        !(num in cache) && (cache[num] = fn.call({ cache }, num))
         
-        // If the value is already registered
-        if(value in cache) {
-            console.log("cache in function",cache)
-            console.log("value already registered",value)
-            return cache[value]
-        } else {
-            // Get the result
-            const result = factorial(value);
-            
-            // Register the value and its result
-            cache[value] = result;
-
-            return result;
-        }
-    
+        return cache[num];
     }
 }
 
-// Here we basically define the scope of the function that has a closure in its block of code
-memoized_func = memFactorial()
+// We can't use arrow fn because we need to make use of `this`
+const factorial = function (num) {
+    return (num in this.cache) 
+        ? this.cache[num]
+        : num === 0 || num === 1
+            ? 1
+            : (this.cache[num] = num * factorial.call(this, num - 1));
+}
 
-// Calling the closure, maintaining the same scope !
-console.log(memoized_func(4))
-console.log(memoized_func(4)) // Here will show a message - value already registered
-console.log(memoized_func(5)) // Here will show a message -  value already registered
-console.log(memoized_func(5))
+const memoizedFactorial = memoize(factorial);
+
+console.log(memoizedFactorial(4))
+console.log(memoizedFactorial(6))
+console.log(memoizedFactorial(5))
+console.log(memoizedFactorial(8))
